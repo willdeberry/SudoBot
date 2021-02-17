fd12643018804e4bdefbce3d657ca9decaee73cf
Will DeBerry <willdeberry@gmail.com>
2021-02-17 02:39:05 +0000

    Add more information to the next game command

--- webhooks/hockey.py
+++ webhooks/hockey.py
@@ -5,14 +5,23 @@ import requests
 
 
 class Hockey:
+    base_url = 'https://statsapi.web.nhl.com'
 
     def tbl_next_game(self):
-        data = requests.get('https://statsapi.web.nhl.com/api/v1/teams/14?expand=team.schedule.next').json()
+        data = requests.get(f'{self.base_url}/api/v1/teams/14?expand=team.schedule.next').json()
         next_game = data['teams'][0]['nextGameSchedule']['dates'][0]['games'][0]
         next_game_date = next_game['gameDate']
         game_time_utc = dateutil.parser.parse(next_game_date)
         game_time_est = game_time_utc.astimezone(pytz.timezone('America/New_York'))
+        teams = next_game['teams']
         venue = next_game['venue']['name']
+        versus = 'Unknown'
+
+        if teams['away']['team']['id'] != 14:
+            versus = teams['away']['team']['name']
+
+        if teams['home']['team']['id'] != 14:
+            versus = teams['home']['team']['name']
 
         return {
             "type": 3,
@@ -26,7 +35,7 @@ class Hockey:
                         "fields": [
                             {
                                 "name": "Date",
-                                "value": f"{game_time_est.strftime('%D %H:%M')} @ {venue}"
+                                "value": f"{game_time_est.strftime('%D %H:%M')} vs {versus} @ {venue}"
                             }
                         ]
                     }
@@ -36,7 +45,7 @@ class Hockey:
         }
 
     def tbl_record(self):
-        data = requests.get('https://statsapi.web.nhl.com/api/v1/teams/14?expand=team.stats').json()
+        data = requests.get(f'{self.base_url}/api/v1/teams/14?expand=team.stats').json()
         stats = data['teams'][0]['teamStats'][0]['splits'][0]['stat']
         games_played = stats['gamesPlayed']
         wins = stats['wins']
