a1df519db9e27873313717be48fafa012bfd6f34
Will DeBerry <willdeberry@gmail.com>
2022-12-08 09:43:00 -0500

    service: fix embed alignment

--- service/utilities/helpers.py
+++ service/utilities/helpers.py
@@ -2,10 +2,11 @@
 import discord
 
 
-def build_embed(title, fields, inline = True):
+def build_embed(title, fields):
     embed = discord.Embed(title = title, type = 'rich')
 
     for field in fields:
+        inline = field.get('inline', False)
         embed.add_field(name = field['name'], value = field['value'], inline = inline)
 
     return embed
